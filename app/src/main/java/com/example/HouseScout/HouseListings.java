8d8ed8f6fae383fb1b3a5d30b77183ee35e9e24e
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2020-10-17 14:48:06 -0700

    Added house xml screen and login logic beginnings

---
+++ app/src/main/java/com/example/HouseScout/HouseListings.java
@@ -0,0 +1,14 @@
+package com.example.HouseScout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HouseListings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_house_listings);
+    }
+}
