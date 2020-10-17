b94fb222ceb813216d5e2d2ba4de9a532e7bde49
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2020-10-17 14:24:06 -0700

    Added initial login screen xml

--- app/src/main/java/com/example/HouseScout/MainActivity.java
+++ app/src/main/java/com/example/HouseScout/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.HouseScout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.HouseScout.R;
 
 public class MainActivity extends AppCompatActivity {
-
+        EditText usernameLogin, passwordLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        passwordLogin = (EditText) findViewById(R.id.UsernameLogin);
+        usernameLogin = (EditText) findViewById(R.id.PasswordLogin);
     }
 }
