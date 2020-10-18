73d6085d7c0b17b82fec4de12340110d81e6faea
Sifat Hasan <sifat120@outlook.com>
2020-10-17 17:30:57 -0700

    Added code to add account into firebase. Still need to add code to check if the account is in the database

--- app/src/main/java/com/example/HouseScout/MainActivity.java
+++ app/src/main/java/com/example/HouseScout/MainActivity.java
@@ -2,16 +2,33 @@ package com.example.HouseScout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
-        EditText usernameLogin, passwordLogin;
+    EditText email, password;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        passwordLogin = (EditText) findViewById(R.id.usernameLogin);
-        usernameLogin = (EditText) findViewById(R.id.passwordLogin);
+
+        email = (EditText) findViewById(R.id.emailLogin);
+        password = (EditText) findViewById(R.id.passwordLogin);
+        fAuth = FirebaseAuth.getInstance();
+    }
+
+    public void checkAccount(View view) {
+        String mail = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+    }
+
+    public void launchRegistrationActivity(View view) {
+        Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/HouseScout/RegistrationActivity.java
+++ app/src/main/java/com/example/HouseScout/RegistrationActivity.java
@@ -1,21 +1,87 @@
 package com.example.HouseScout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = RegistrationActivity.class.getSimpleName();
+    EditText mfName, mlName, mEmail, mPassword, mPasswordC;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        mfName = findViewById(R.id.firstNameInput);
+        mlName = findViewById(R.id.lastNameInput);
+        mEmail = findViewById(R.id.emailInput);
+        mPassword = findViewById(R.id.passwordInput);
+        mPasswordC = findViewById(R.id.passwordInput2);
+        fAuth.getInstance();
+
         Toolbar registrationToolbar = (Toolbar) findViewById(R.id.registrationToolbar);
         setSupportActionBar(registrationToolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void registerAccount(View view) {
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String passwordC = mPasswordC.getText().toString().trim();
+        String fName = mfName.getText().toString().trim();
+        String lName = mlName.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            mEmail.setError("Your email is required.");
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            mEmail.setError("Your password is required.");
+            return;
+        }
+        if(TextUtils.isEmpty(passwordC)){
+            mPasswordC.setError("Your confirmed password is required.");
+            return;
+        }
+        if(TextUtils.isEmpty(fName)){
+            mfName.setError("Your first name is required.");
+            return;
+        }
+        if(TextUtils.isEmpty(lName)){
+            mlName.setError("Your last name is required.");
+            return;
+        }
+        if(!passwordC.equals(password)){
+            mPasswordC.setError("Both passwords don't match. Please try again.");
+            return;
+        }
+        fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(RegistrationActivity.this,"Account created.",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                } else {
+                    Toast.makeText(RegistrationActivity.this,"Error Occurred. Error: " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
