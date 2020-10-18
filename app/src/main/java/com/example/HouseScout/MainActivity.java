67ef9aca98e04e4f99c0c2bb057dd0427c6dbdc5
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2020-10-17 23:23:22 -0700

    Added firebase functionality

--- app/src/main/java/com/example/HouseScout/MainActivity.java
+++ app/src/main/java/com/example/HouseScout/MainActivity.java
@@ -1,19 +1,54 @@
 package com.example.HouseScout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    EditText email, password;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        email = findViewById(R.id.emailLogin);
+        password = findViewById(R.id.passwordLogin);
+        fAuth = FirebaseAuth.getInstance();
     }
 
+    public void checkAccount(View view) {
+        String mail = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        fAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(getApplicationContext(),HouseListing.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Information is Incorrect. Please Try Again.", Toast.LENGTH_SHORT).show();
+                    Log.d(LOG_TAG,task.getException().toString());
+                }
+            }
+        });
+    }
+
+    public void launchRegistrationActivity(View view) {
+        Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/HouseScout/RegistrationActivity.java
+++ app/src/main/java/com/example/HouseScout/RegistrationActivity.java
@@ -1,21 +1,100 @@
 package com.example.HouseScout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = RegistrationActivity.class.getSimpleName();
+    EditText mfName, mlName, mEmail, mPassword, mPasswordC;
+    Button registerButton;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        fAuth = FirebaseAuth.getInstance();
+        mfName = findViewById(R.id.firstNameInput);
+        mlName = findViewById(R.id.lastNameInput);
+        mEmail = findViewById(R.id.emailInput);
+        mPassword = findViewById(R.id.passwordInput);
+        mPasswordC = findViewById(R.id.passwordInput2);
+        registerButton = findViewById(R.id.registerButton);
+
         Toolbar registrationToolbar = (Toolbar) findViewById(R.id.registrationToolbar);
         setSupportActionBar(registrationToolbar);
         ActionBar ab = getSupportActionBar();
+        assert ab != null;
         ab.setDisplayHomeAsUpEnabled(true);
+
+        registerButton.setOnClickListener(view -> {
+            String email = mEmail.getText().toString().trim();
+            String password = mPassword.getText().toString().trim();
+            String passwordC = mPasswordC.getText().toString().trim();
+            String fName = mfName.getText().toString().trim();
+            String lName = mlName.getText().toString().trim();
+
+            if(TextUtils.isEmpty(email)){
+                mEmail.setError("Your email is required.");
+                return;
+            }
+            if(TextUtils.isEmpty(password)){
+                mEmail.setError("Your password is required.");
+                return;
+            }
+            if(TextUtils.isEmpty(passwordC)){
+                mPasswordC.setError("Your confirmed password is required.");
+                return;
+            }
+            if(TextUtils.isEmpty(fName)){
+                mfName.setError("Your first name is required.");
+                return;
+            }
+            if(TextUtils.isEmpty(lName)){
+                mlName.setError("Your last name is required.");
+                return;
+            }
+            if(!passwordC.equals(password)){
+                mPasswordC.setError("Both passwords don't match. Please try again.");
+                return;
+            }
+            if(password.length() < 6){
+                mPassword.setError("Password needs to be longer than 6 characters");
+            }
+            fAuth.createUserWithEmailAndPassword(email, passwordC).addOnCompleteListener(RegistrationActivity.this, task -> {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(getApplicationContext(),HouseListing.class);
+                    startActivity(intent);
+                } else {
+                    Log.d(LOG_TAG,task.getException().toString());
+                }
+            });
+            Log.d(LOG_TAG,"Email:"+email);
+            Log.d(LOG_TAG,"Password:"+password);
+            Log.d(LOG_TAG,"PasswordC:"+passwordC);
+            Log.d(LOG_TAG,"First name:"+fName);
+            Log.d(LOG_TAG,"Last name:"+lName);
+        });
     }
 }
